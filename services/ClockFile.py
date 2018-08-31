f7a9414e2ca39d8d362314af8c22d53da6b54b09
Matthew Splett <Matthew Splett>
2018-08-30 18:22:18 -0700

    by default return corcomp clock data.

--- services/ClockFile.py
+++ services/ClockFile.py
@@ -1,5 +1,6 @@
 
 import time
+import logging
 from Pab import *
 
 
@@ -21,38 +22,42 @@ class ClockFile(object):
             self.close(pab, devname)
         elif op == READ:
             self.read(pab, devname)
+        elif op == WRITE:
+            self.write(pab, devname)
         else:
             self.tipi_io.send([EOPATTR])
 
     def close(self, pab, devname):
-        print "close special? {}".format(devname)
         self.tipi_io.send([SUCCESS])
         self.mode = 0
 
     def open(self, pab, devname):
-        if mode(pab) == INPUT:
-            if dataType(pab) == DISPLAY:
-                if recordLength(pab) == 0 or recordLength(pab) == 19:
-                    self.tipi_io.send([SUCCESS])
-                    self.tipi_io.send([19])
-                    self.mode = 19
-                    return
-                if recordLength(pab) == 24:
-                    self.tipi_io.send([SUCCESS])
-                    self.tipi_io.send([24])
-                    self.mode = 24
-                    return
-        self.tipi_io.send([EOPATTR])
-
-    def read(self, pab, devname):
-        if mode(pab) == INPUT:
-            if dataType(pab) == DISPLAY:
-                fdata = self.getTime()
+        if dataType(pab) == DISPLAY:
+            if recordLength(pab) == 0 or recordLength(pab) == 19:
                 self.tipi_io.send([SUCCESS])
-                self.tipi_io.send(fdata)
+                self.tipi_io.send([19])
+                self.mode = 19
+                return
+            if recordLength(pab) == 24:
+                self.tipi_io.send([SUCCESS])
+                self.tipi_io.send([24])
+                self.mode = 24
                 return
         self.tipi_io.send([EOPATTR])
 
+    def read(self, pab, devname):
+        if dataType(pab) == DISPLAY:
+            fdata = self.getTime()
+            self.tipi_io.send([SUCCESS])
+            self.tipi_io.send(fdata)
+            return
+        self.tipi_io.send([EOPATTR])
+
+    def write(self, pab, devname):
+        # ignore, by dropping the data.
+        self.tipi_io.send([SUCCESS])
+        self.tipi_io.receive()
+        self.tipi_io.send([SUCCESS])
 
     def getTime(self):
         if mode == 24:
